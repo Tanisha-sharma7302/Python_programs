5931734d8f3591f1c8575170dded18a85c4f1eb4
Tanisha-sharma7302 <sharmatanisha7302@gmail.com>
2025-11-21 08:45:46 +0530

    Delete sheet3_patterns directory

--- sheet3_patterns/pattern1.py
+++
@@ -1,4 +0,0 @@
-for i in range(0,4):
-    for j in range(0,5):
-        print("*",end=" ")
-    print()
--- sheet3_patterns/pattern6.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,6):#row1
-    for j in range(i,8):
-        if(j==i or j==7):
-            print("*",end=" ")
-        else:
-            print("_",end=" ")
-    print()
-
