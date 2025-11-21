b123511cc9a087cae9dff88223a1837d796747f4
Tanisha-sharma7302 <sharmatanisha7302@gmail.com>
2025-11-21 08:46:04 +0530

    Delete sheet4_patterns directory

--- sheet4_patterns/pattern14.py
+++
@@ -1,8 +0,0 @@
-for i in range(5,0,-1):
-    for j in range(i):
-        print("*",end="")
-    print()
-for i in range(1,5):
-    for j in range(i+1):
-        print("*",end="")
-    print()
--- sheet4_patterns/pattern7.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,6):
-    for j in range(5):
-        if(i==1 or i==5) or (j>2):
-            print("*",end="")
-        else:
-            print("",end="")
-    print()
-
